cb696dbb16947d9f4cb9a72238910528a181a9d5
juan carlos <jcjuradoarmas@gmail.com>
2015-09-09 23:55:38 -0500

    Version inicial

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>IPG</title>
+
+    
+<link href="libreria/estilo.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    esto es mi index
+</body>
+</html>
