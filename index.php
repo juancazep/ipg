0937147a823a19798d3464ed3debb135dbd9d358
juan carlos <jcjuradoarmas@gmail.com>
2015-09-10 00:23:31 -0500

    bootstrap y imagenes

--- index.php
+++ index.php
@@ -4,10 +4,24 @@
     <meta charset="UTF-8">
     <title>IPG</title>
 
-    
+    <link href="libreria/bootstrap/css/bootstrap.css" type="text/css" rel="stylesheet" />    
 <link href="libreria/estilo.css" type="text/css" rel="stylesheet" />
 </head>
 <body>
+    <header>
+        <h1>My web site</h1>
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="col-sx-12">
+                <p>aca el contenido</p>
+            </div>
+            <div class="col-lg-1">
+                <img src="imagenes/blue.jpg" class="responsive"/>
+            </div>
+        </div>
+        
+    </div>
     esto es mi index
 </body>
 </html>
